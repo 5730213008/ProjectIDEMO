797566101e4f8ac11353fa1cf8dfa4a2576d14c9
5730213008 <s5730213008@phuket.psu.ac.th>
2017-05-14 13:50:55 +0700

    add

---
+++ ProjectIDEMO/upload.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="upload.aspx.cs" Inherits="ProjectIDEMO.upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="ชื่อ"></asp:Label>
+        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="รายละเอียด"></asp:Label>
+        <asp:TextBox ID="txtdes" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="เลือก"></asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="ปรเภทเอกสาร"></asp:Label>
+        <asp:DropDownList ID="ddldoctype" runat="server">
+            <asp:ListItem>กรุณาเลือกประเภทเอกสาร</asp:ListItem>
+            <asp:ListItem>ประกาศ</asp:ListItem>
+            <asp:ListItem>แบบฟอร์ม</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="วันที่อัพโหลด"></asp:Label>
+        <asp:TextBox ID="txtdateupload" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="วันที่หมดอายุ"></asp:Label>
+        <asp:TextBox ID="txtdateexpire" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButtonList ID="optFundtype" runat="server" AutoPostBack="True" RepeatDirection="Horizontal">
+            <asp:ListItem>กยศ</asp:ListItem>
+            <asp:ListItem>กรอ</asp:ListItem>
+            <asp:ListItem>ทุนภายนอก</asp:ListItem>
+            <asp:ListItem>ทุนภายใน</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+        <br />
+        <asp:Label ID="lberror" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:HyperLinkField DataNavigateUrlFields="name" DataNavigateUrlFormatString="~/uploadfile{0}" DataTextField="name" HeaderText="Filename" />
+            </Columns>
+        </asp:GridView>
+        <br />
+    </form>
+</body>
+</html>
---
+++ ProjectIDEMO/upload.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectIDEMO
+{
+    public partial class upload : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentFundConnectionString"].ConnectionString);
+        SqlCommand cmd = null;
+        SqlDataReader dr = null;
+        string type;
+        string filename;
+        string fundtypes;
+        int fsize;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                GridDisplayFile();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            fundtypes = optFundtype.SelectedValue;
+            doctype();
+            
+            if (FileUpload1.HasFile)
+            {
+                try
+                {
+                    filename = Path.GetFileName(FileUpload1.FileName);
+                    fsize = FileUpload1.PostedFile.ContentLength;
+                    FileUpload1.SaveAs(Server.MapPath("~/uploadfile////" + filename));
+                    //FileUpload1.SaveAs(Server.MapPath("c: \\users\\5730213008\\documents\\visual studio 2015\\Projects\\ProjectIDEMO\\ProjectIDEMO\\uploadfile\\") + filename);
+                    lberror.Text = "Upload status: File uploaded!";
+                    InsertFile();
+                    
+                }
+
+                catch (Exception ex)
+                {
+                    lberror.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+
+        private void InsertFile()
+        {
+            con.Open();
+            cmd = new SqlCommand("INSERT INTO FileDetials(name,filedescription,doc_type,fund_type,size) VALUES('" + filename + "','" + txtdes.Text + "','" + type + "','" + fundtypes + "','" +fsize/1024+ "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            GridDisplayFile();
+        }
+
+        private void GridDisplayFile()
+        {
+            con.Open();
+            cmd = new SqlCommand("SELECT * FROM FileDetials",con);
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+            }
+            else
+            {
+                lberror.Text = "Nothing to availible";
+            }
+        }
+
+        public void doctype()
+        {
+
+            if (ddldoctype.SelectedIndex == 1)
+            {
+                type = ddldoctype.SelectedItem.Text;
+            }
+            else if (ddldoctype.SelectedIndex == 2)
+            {
+                type = ddldoctype.SelectedItem.Text;
+            }
+        }
+    }
+}
